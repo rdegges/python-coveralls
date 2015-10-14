2de18c0eb5b9433774c06a6689112303aea1aa15
Randall Degges <r@rdegges.com>
2015-10-14 12:22:23 -0700

    Updating broken code sample.

--- coveralls/control.py
+++ coveralls/control.py
@@ -5,5 +5,5 @@ from coveralls.report import CoverallsReporter
 class coveralls(Coverage):
     def coveralls(self, base_dir, ignore_errors=False, merge_file=None):
         reporter = CoverallsReporter(self, self.config)
-        reporter.find_code_units(None)
+        reporter.find_file_reporters(None)
         return reporter.report(base_dir, ignore_errors=ignore_errors, merge_file=merge_file)
